9d5051d4ea1888a884e125774164b4f19bad54d0
Martin Halamíček <martin@halamicek.cz>
2015-05-28 17:44:04 +0200

    feat(Clipboard): copy and copied tooltip SOLVES #70

--- src/scripts/react/common/Clipboard.jsx
+++ src/scripts/react/common/Clipboard.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactZeroClipboard from 'react-zeroclipboard';
+import {Tooltip, OverlayTrigger} from 'react-bootstrap';
 
 export default React.createClass({
   propTypes: {
     text: React.PropTypes.string.isRequired
   },
 
+  getInitialState() {
+    return {
+      isCopied: false
+    };
+  },
+
   render() {
     return (
-      <ReactZeroClipboard getText={this.props.text}>
-        <span className="fa fa-fw fa-copy" />
-      </ReactZeroClipboard>
+      <OverlayTrigger overlay={this.tooltip()} ref="overlay">
+        <span>
+          <ReactZeroClipboard getText={this.props.text} onAfterCopy={this.handleAfterCopy}>
+            <span className="fa fa-fw fa-copy" />
+          </ReactZeroClipboard>
+        </span>
+      </OverlayTrigger>
     );
+  },
+
+  tooltip() {
+    return (
+      <Tooltip>{this.state.isCopied ? 'Copied!' : 'Copy to clipboard'}</Tooltip>
+    );
+  },
+
+  handleAfterCopy() {
+    this.setState({
+      isCopied: true
+    });
+    this.refs.overlay.show();
+    /*global setTimeout*/
+    setTimeout(this.hideOverlay, 300);
+  },
+
+  hideOverlay() {
+    this.refs.overlay.hide();
+    this.setState({
+      isCopied: false
+    });
   }
 
 });
