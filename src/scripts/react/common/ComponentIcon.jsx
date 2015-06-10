2f8089f142aa11091eb83fbddbc4e332ff923d4e
Martin Halamíček <martin@halamicek.cz>
2015-06-10 14:00:11 +0200

    feat: images for default components FIXES #110

--- src/scripts/react/common/ComponentIcon.jsx
+++ src/scripts/react/common/ComponentIcon.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const DEFAULT_ICON_CLASS = {
-  extractor: 'fa-cloud-download',
-  writer: 'fa-cloud-upload',
-  transformation: 'fa-cogs',
-  recipe: 'fa-cogs',
-  other: 'fa-cogs'
+import extractor from '../../../images/extractor-32-1.png';
+import writer from '../../../images/writer-32-1.png';
+import other from '../../../images/other-32-1.png';
+
+const DEFAULT_ICON_IMAGES = {
+  extractor: extractor,
+  writer: writer,
+  other: other
 };
+
+function getComponentIconURL(componentType) {
+  return DEFAULT_ICON_IMAGES[componentType] ? DEFAULT_ICON_IMAGES[componentType] : DEFAULT_ICON_IMAGES.other;
+}
 
 export default React.createClass({
   propTypes: {
@@ -27,7 +33,9 @@ export default React.createClass({
     if (!component) {
       return this.emptyIcon();
     }
-    return component.get('ico' + this.props.size) ? this.imageIcon() : this.fontIcon();
+    return component.get('ico' + this.props.size) ?
+      this.imageIcon(this.props.component.get('ico' + this.props.size)) :
+      this.imageIcon(getComponentIconURL(this.props.component.get('type')));
   },
 
   emptyIcon() {
@@ -36,28 +44,10 @@ export default React.createClass({
     );
   },
 
-  fontIcon() {
-    const iconStyle = {
-      'font-size': (this.props.size - 5) + 'px',
-      height: this.props.size + 'px',
-      position: 'relative',
-      top: '5px'
-    };
+  imageIcon(url) {
     return (
       <span className={classNames('kb-sapi-component-icon', this.props.className)}>
-        <span className="kb-default">
-          <i className={classNames('fa', DEFAULT_ICON_CLASS[this.props.component.get('type')])}
-            style={iconStyle}
-            />
-        </span>
-      </span>
-    );
-  },
-
-  imageIcon() {
-    return (
-      <span className={classNames('kb-sapi-component-icon', this.props.className)}>
-        <img src={this.props.component.get('ico' + this.props.size)} />
+        <img src={ url } />
       </span>
     );
   }
