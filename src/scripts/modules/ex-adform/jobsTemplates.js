4b7e565710dc270d4c7005987bd47651e51bdecd
Martin Halamíček <martin@halamicek.cz>
2015-09-04 17:55:51 +0200

    feat(ex-adform): new template

--- src/scripts/modules/ex-adform/jobsTemplates.js
+++ src/scripts/modules/ex-adform/jobsTemplates.js
@@ -47,6 +47,89 @@ export default fromJS([
     ]
   },
   {
+    id: 'All',
+    name: 'Full - Advertisers, Campaigns, Campaign Stats, Users',
+    template: [
+      {
+        'endpoint': 'Advertiser/Advertisers',
+        'params': [],
+        'dataType': 'advertisers',
+        'dataField': 'Advertisers',
+        'children': [
+          {
+            'endpoint': 'Campaign/Campaigns?AdvertiserId={advertiserId}',
+            'params': {
+              'DateFrom': {
+                'function': 'date',
+                'args': [
+                  'Y-m-d',
+                  {
+                    'time': 'previousStart'
+                  }
+                ]
+              },
+              'DateTo': {
+                'function': 'date',
+                'args': [
+                  'Y-m-d',
+                  {
+                    'function': 'strtotime',
+                    'args': [
+                      '-1 day'
+                    ]
+                  }
+                ]
+              }
+            },
+            'dataType': 'campaigns',
+            'dataField': 'Campaigns',
+            'placeholders': {
+              'advertiserId': 'Id'
+            },
+            'children': [
+              {
+                'endpoint': 'CampaignStats/AdStats?campaignId={campaignId}',
+                'params': {
+                  'StartDate': {
+                    'function': 'date',
+                    'args': [
+                      'Y-m-d',
+                      {
+                        'time': 'previousStart'
+                      }
+                    ]
+                  },
+                  'EndDate': {
+                    'function': 'date',
+                    'args': [
+                      'Y-m-d',
+                      {
+                        'function': 'strtotime',
+                        'args': [
+                          '-1 day'
+                        ]
+                      }
+                    ]
+                  }
+                },
+                'dataType': 'adstats',
+                'dataField': 'Campaign.Ads',
+                'placeholders': {
+                  'campaignId': 'Id'
+                }
+              }
+            ]
+          }
+        ]
+      },
+      {
+        'endpoint': 'User/Users',
+        'dataType': 'users',
+        'dataField': 'Users'
+      }
+    ]
+  },
+  {
     id: 'Empty',
     name: 'Empty template',
     template: []
