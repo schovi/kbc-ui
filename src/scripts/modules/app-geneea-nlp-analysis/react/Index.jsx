88ff2e77c6c03f3fae805bd37f04a4a1b790021c
Tomas Kacur <tomas.kacur@keboola.com>
2015-09-08 18:15:44 +0200

    feat(geneea-general): basic layout

--- src/scripts/modules/app-geneea-nlp-analysis/react/Index.jsx
+++ src/scripts/modules/app-geneea-nlp-analysis/react/Index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import installedComponentsActions from '../../components/InstalledComponentsActionCreators';
+
 import createStoreMixin from '../../../react/mixins/createStoreMixin';
 import RoutesStore from '../../../stores/RoutesStore';
 import InstalledComponentStore from '../../components/stores/InstalledComponentsStore';
@@ -7,27 +9,55 @@ import ComponentStore from '../../components/stores/ComponentsStore';
 import LatestJobsStore from '../../jobs/stores/LatestJobsStore';
 
 //import EmptyState from '../../components/react/components/ComponentEmptyState';
-//import ComponentDescription from '../../components/react/components/ComponentDescription';
+import ComponentDescription from '../../components/react/components/ComponentDescription';
 //import ComponentMetadata from '../../components/react/components/ComponentMetadata';
 //import RunComponentButton from '../../components/react/components/RunComponentButton';
 //import DeleteConfigurationButton from '../../components/react/components/DeleteConfigurationButton';
 //import LatestJobs from '../../components/react/components/SidebarJobs';
 
+const componentId = 'geneea-nlp-analysis';
 
 export default React.createClass({
   mixins: [createStoreMixin(InstalledComponentStore, LatestJobsStore, ComponentStore)],
 
   getStateFromStores(){
     const configId = RoutesStore.getCurrentRouteParam('config');
+    const localState = InstalledComponentStore.getLocalState(componentId, configId);
+
     return {
-      configId: configId
+      configId: configId,
+      localState: localState
     };
   },
 
 
   render(){
-    return (<div> INDEX </div>);
-  }
+    return (
+      <div className="container-fluid">
+        <div className="col-md-9 kbc-main-content">
+          <div className="row kbc-header">
+            <ComponentDescription
+              componentId={componentId}
+              configId={this.state.configId}
+              />
+          </div>
+          <div className="row">
+            <div className="col-xs-4">
+              <div>
+                main content
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
+    );
+  },
+
+  updateLocalState(path, data){
+    const newState = this.state.localState.setIn(path, data);
+    installedComponentsActions.updateLocalState(componentId, this.state.configId, newState);
+
+  }
 
 });
