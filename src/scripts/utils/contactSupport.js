d098c6cf4f32f49696847e616d61ffa3f204dd7a
Martin Halamíček <martin@halamicek.cz>
2015-09-08 09:41:24 +0200

    fix: contact support cleanup

--- src/scripts/utils/contactSupport.js
+++ src/scripts/utils/contactSupport.js
@@ -1,12 +1,5 @@
-import ApplicationStore from '../stores/ApplicationStore';
 
-function webWidget() {
-  /*global zE*/
-  zE.activate({hideOnClose: true});
-}
-
-export default function({type = 'project'}) {
-  const settings = ApplicationStore.getKbcVars().getIn(['zendesk', type]);
+export default function() {
   /*global zE*/
   zE.activate({hideOnClose: true});
 }
