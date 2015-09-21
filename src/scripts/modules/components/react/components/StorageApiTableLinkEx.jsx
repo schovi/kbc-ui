8be9975b4f2394fab06d0cfa91487b95e9703ab9
Tomas Kacur <tomas.kacur@keboola.com>
2015-09-21 10:54:04 +0200

    feat(tablLinkEx): modal, load table and show attributes

---
+++ src/scripts/modules/components/react/components/StorageApiTableLinkEx.jsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import {Modal, Button} from 'react-bootstrap';
+import Tooltip from '../../../../react/common/Tooltip';
+import Loader from 'kbc-react-components';
+import date from '../../../../utils/date';
+import filesize from 'filesize';
+
+import storageActions from '../../StorageActionCreators';
+
+import tablesStore from '../../stores/StorageTablesStore';
+import createStoreMixin from '../../../../react/mixins/createStoreMixin';
+
+export default React.createClass({
+
+  mixins: [createStoreMixin(tablesStore)],
+
+  propTypes: {
+    tableId: React.PropTypes.string.isRequired,
+    linkLabel: React.PropTypes.string
+  },
+
+  getStateFromStores(){
+    const isLoading = tablesStore.getIsLoading();
+    let table = null;
+    if(!isLoading){
+      table = tablesStore.getAll().get(this.props.tableId);
+    }
+    return {
+      table: table,
+      isLoading: isLoading
+    };
+  },
+
+  componentDidMount(){
+    storageActions.loadTables();
+  },
+
+  getInitialState(){
+    return ({show: false});
+  },
+
+  render(){
+    return (
+      <span>
+        <Tooltip
+          tooltip={this.renderTooltip()}
+          placement="top">
+          {this.renderLink()}
+        </Tooltip>
+        {this.renderModal()}
+      </span>
+    );
+
+  },
+
+  renderLink(){
+    return (
+      <Button
+        bsStyle="link"
+        onClick={this.onShow}>
+        {this.props.linkLabel || this.props.tableId}
+      </Button>);
+
+  },
+
+  renderModalBody(){
+    const table = this.state.table;
+
+    return (
+      <div>
+        <table className="table table-stripped">
+          <tbody>
+            {this.renderTableRow('Created', date.format(table.get('created')))}
+            {this.renderTableRow('Last Import', date.format(table.get('lastImportDate')))}
+            {this.renderTableRow('Last Change', date.format(table.get('lastChangeDate')))}
+            {this.renderTableRow('Rows Count', table.get('rowsCount') + ' rows')}
+            {this.renderTableRow('Columns Count', table.get('columns').count() + ' columns')}
+          </tbody>
+        </table>
+      </div>
+    );
+  },
+
+  renderTableRow(name, value){
+    return (
+      <tr>
+        <td>
+          {name}
+        </td>
+        <td>
+          {value}
+        </td>
+      </tr>
+    );
+
+  },
+
+  renderModal(){
+    let modalBody = null;
+    if (this.state.isLoading){
+      modalBody = (<Loader />);
+    }
+    else{
+      modalBody = this.renderModalBody();
+    }
+
+    return (
+      <div className="static-modal">
+        <Modal
+          show={this.state.show}
+          onHide={this.onHide}
+          >
+          <Modal.Header closeButton>
+            <Modal.Title>{this.props.tableId}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {modalBody}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.onHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  },
+
+
+  renderTooltip(){
+    const table = this.state.table;
+    return (
+      <span>
+        <div>
+          {date.format(table.get('lastChangeDate'))}
+        </div>
+        <div>
+          {filesize(table.get('dataSizeBytes'))}
+        </div>
+        <div>
+          {table.get('rowsCount')} rows
+        </div>
+      </span>
+    );
+  },
+
+  onHide(){
+    this.setState({show: false});
+  },
+
+  onShow(){
+    this.setState({show: true});
+  }
+
+
+
+
+});
