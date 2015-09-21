f5e7ba16fd0433c79bcbdb85cec10eb8f5b7ba42
Tomas Kacur <tomas.kacur@keboola.com>
2015-09-21 16:53:30 +0200

    feat(sapiTableLinkEx): columns samples

--- src/scripts/modules/components/react/components/StorageApiTableLinkEx.jsx
+++ src/scripts/modules/components/react/components/StorageApiTableLinkEx.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Immutable from 'immutable';
+import _ from 'underscore';
+
 import {Modal, Button} from 'react-bootstrap';
+import {TabbedArea, TabPane} from 'react-bootstrap';
 import Tooltip from '../../../../react/common/Tooltip';
-import Loader from 'kbc-react-components';
+import {Loader} from 'kbc-react-components';
 import date from '../../../../utils/date';
 import filesize from 'filesize';
 
 import storageActions from '../../StorageActionCreators';
+import storageApi from '../../StorageApi';
 
 import tablesStore from '../../stores/StorageTablesStore';
 import createStoreMixin from '../../../../react/mixins/createStoreMixin';
@@ -32,11 +37,29 @@ export default React.createClass({
   },
 
   componentDidMount(){
-    storageActions.loadTables();
+    storageActions.loadTables().then(() => this.exportDataSample());
+
+  },
+
+  exportDataSample(){
+    const component = this;
+    storageApi
+    .exportTable(this.props.tableId, {limit: 10})
+    .then( (csv) => {
+      component.setState({
+        loadingPreview: false,
+        dataPreview: csv
+      });
+    });
+
   },
 
   getInitialState(){
-    return ({show: false});
+    return ({
+      show: false,
+      dataPreview: null,
+      loadingPreview: true
+    });
   },
 
   render(){
@@ -64,17 +87,73 @@ export default React.createClass({
   },
 
   renderModalBody(){
-    const table = this.state.table;
+    return (
+      <TabbedArea animation={false}>
+        <TabPane eventKey="general" tab="General Info">
+          {this.renderGeneralInfo()}
+        </TabPane>
+        <TabPane eventKey="columns" tab="Columns">
+          {this.renderColumnsInfo()}
+        </TabPane>
+      </TabbedArea>
+    );
+
+  },
+
+  renderColumnsInfo(){
+    if (this.state.loadingPreview){
+      return (
+        <div>
+          <Loader/>
+        </div>
+      );
+    }
+    const {table} = this.state;
+    const columns = table.get('columns').map((c) => {
+      const values = this.getColumnValues(c);
+      return this.renderTableRow(c, values.join(' ,'));
+    });
 
     return (
       <div>
         <table className="table table-stripped">
+          <thead>
+            <tr>
+              <th>
+                Column
+              </th>
+              <th>
+                Sample Values
+              </th>
+            </tr>
+          </thead>
           <tbody>
+            {columns}
+          </tbody>
+        </table>
+      </div>
+    );
+  },
+
+  renderGeneralInfo(){
+    const table = this.state.table;
+    const primaryKey = table.get('primaryKey').toJS();
+    const indexes = table.get('indexedColumns').toJS();
+    return (
+      <div>
+        <table className="table table-stripped">
+          <tbody>
+            {this.renderTableRow('ID', table.get('id'))}
             {this.renderTableRow('Created', date.format(table.get('created')))}
+            {this.renderTableRow('Primary Key', _.isEmpty(primaryKey) ? 'N/A' : primaryKey.join(','))}
             {this.renderTableRow('Last Import', date.format(table.get('lastImportDate')))}
             {this.renderTableRow('Last Change', date.format(table.get('lastChangeDate')))}
+
             {this.renderTableRow('Rows Count', table.get('rowsCount') + ' rows')}
+            {this.renderTableRow('Data Size', filesize(table.get('dataSizeBytes')))}
             {this.renderTableRow('Columns Count', table.get('columns').count() + ' columns')}
+
+            {this.renderTableRow('Indexed Column(s)', _.isEmpty(indexes) ? 'N/A' : indexes.join(' ,'))}
           </tbody>
         </table>
       </div>
@@ -148,6 +227,19 @@ export default React.createClass({
 
   onShow(){
     this.setState({show: true});
+  },
+
+
+  getColumnValues(columnName){
+    const data = Immutable.fromJS(this.state.dataPreview);
+    const columnIndex = data.first().indexOf(columnName);
+
+    const result = data
+    .shift()
+    .map( (row) => {
+      return row.get(columnIndex);
+    });
+    return result;
   }
 
 
