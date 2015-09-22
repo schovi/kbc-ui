e4d8a5018561f3e1972c06d5a69b5f5bba01cd44
Tomas Kacur <tomas.kacur@keboola.com>
2015-09-22 16:15:20 +0200

    feat(SapiTableLinkEx): import/export events tab, scrollable data sample tab

--- src/scripts/modules/components/react/components/StorageApiTableLinkEx.jsx
+++ src/scripts/modules/components/react/components/StorageApiTableLinkEx.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import Immutable from 'immutable';
 import _ from 'underscore';
+import Promise from 'bluebird';
 
-import {Modal, Button} from 'react-bootstrap';
+import {Table, Modal, Button} from 'react-bootstrap';
 import {TabbedArea, TabPane} from 'react-bootstrap';
+import filesize from 'filesize';
+import {RefreshIcon} from 'kbc-react-components';
+
 import Tooltip from '../../../../react/common/Tooltip';
 import {Loader} from 'kbc-react-components';
 import SapiTableLink from './StorageApiTableLink';
 import date from '../../../../utils/date';
-import filesize from 'filesize';
+
 
 import storageActions from '../../StorageActionCreators';
 import storageApi from '../../StorageApi';
 
 import tablesStore from '../../stores/StorageTablesStore';
 import createStoreMixin from '../../../../react/mixins/createStoreMixin';
+
+import EventsService from '../../../sapi-events/EventService';
 
 export default React.createClass({
 
@@ -37,28 +43,28 @@ export default React.createClass({
     };
   },
 
+
   componentDidMount(){
-    storageActions.loadTables().then(() => this.exportDataSample());
-
+    storageActions.loadTables();//.then(() => this.exportDataSample());
   },
 
-  exportDataSample(){
-    const component = this;
-    storageApi
-    .exportTable(this.props.tableId, {limit: 10})
-    .then( (csv) => {
-      component.setState({
-        loadingPreview: false,
-        dataPreview: csv
-      });
-    });
-
+  componentWilUnmount(){
+    this.stopEventService();
   },
+
+
 
   getInitialState(){
+    const es = EventsService.factory({limit: 10});
+    let typesQuery = _.map(_.keys(this.eventsTemplates), (t) => 'event:' + t).join(' OR ');
+    typesQuery = `(${typesQuery}) AND objectId:${this.props.tableId}`;
+    es.setQuery(typesQuery);
+
     return ({
+      eventService: es,
+      events: Immutable.List(),
       show: false,
-      dataPreview: null,
+      dataPreview: Immutable.List(),
       loadingPreview: true
     });
   },
@@ -74,7 +80,6 @@ export default React.createClass({
         {this.renderModal()}
       </span>
     );
-
   },
 
   renderLink(){
@@ -96,28 +101,138 @@ export default React.createClass({
         <TabPane eventKey="columns" tab="Columns">
           {this.renderColumnsInfo()}
         </TabPane>
+        <TabPane eventKey="datasample" tab="Data Sample">
+          {this.renderDataSample()}
+        </TabPane>
+        <TabPane eventKey="events" tab="Export/Import Events">
+          {this.renderEvents()}
+        </TabPane>
       </TabbedArea>
     );
 
   },
 
-  renderColumnsInfo(){
+  renderEvents(){
+    /* if (this.state.eventService.getIsLoading()){
+       return (
+       <div className="row">
+       <Loader/>
+       </div>
+       );
+       } */
+
+    //console.log(this.state.events.toJS(), this.state.events.count());
+    const events = this.state.events;
+    const rows = events.map( (e) => {
+      const info = this.eventsTemplates[e.get('event')];
+      const cl = `tr ${info.className}`;
+
+      return (
+        <tr className={cl}>
+          <td className="td">
+            {date.format(e.get('created'))}
+          </td>
+          <td className="td">
+            {e.get('component')}
+          </td>
+          <td className="td">
+            {info.message}
+          </td>
+          <td className="td">
+            {e.getIn(['token', 'name'])}
+          </td>
+        </tr>
+      );
+    }
+    );
+    return (
+      <table className="table table-striped">
+        <thead className="thead">
+          <tr className="tr">
+            <th className="th">
+              Created
+            </th>
+            <th className="th">
+              Component
+            </th>
+            <th className="th">
+              Event
+            </th>
+            <th className="th">
+              Creator
+            </th>
+
+          </tr>
+        </thead>
+        <tbody className="tbody">
+          {rows}
+        </tbody>
+      </table>);
+  },
+
+  renderDataSample(){
     if (this.state.loadingPreview){
       return (
-        <div>
+        <div className="row">
           <Loader/>
         </div>
       );
     }
-    const {table} = this.state;
-    const columns = table.get('columns').map((c) => {
-      const values = this.getColumnValues(c);
-      return this.renderTableRow(c, values.join(' ,'));
+
+    const data = Immutable.fromJS(this.state.dataPreview);
+    const header = data.first().map( (c) => {
+      return (
+        <th>
+          {c}
+        </th>
+      );
+    }).toArray();
+    const rows = data.rest().map( (row) => {
+      const cols = row.map( (c) => {
+        return (<td> {c} </td>);
+      });
+
+      return (
+        <tr>
+          {cols}
+        </tr>);
     });
 
     return (
       <div>
-        <table className="table table-stripped">
+        <Table responsive className="table table-stripped">
+          <thead>
+            <tr>
+              {header}
+            </tr>
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </Table>
+      </div>
+    );
+  },
+
+  renderColumnsInfo(){
+    if (this.state.loadingPreview){
+      return (
+        <div className="row">
+          <Loader/>
+        </div>
+      );
+    }
+
+    const {table} = this.state;
+    const columns = table.get('columns').map((c) => {
+      const values = this.getColumnValues(c);
+      let result = values.filter((val) => val !== '').join(' ,');
+      return this.renderTableRow(c, result);
+    });
+
+    return (
+      <div>
+        <Table responsive className="table table-stripped">
           <thead>
             <tr>
               <th>
@@ -131,7 +246,7 @@ export default React.createClass({
           <tbody>
             {columns}
           </tbody>
-        </table>
+        </Table>
       </div>
     );
   },
@@ -187,6 +302,7 @@ export default React.createClass({
     return (
       <div className="static-modal">
         <Modal
+          bsSize="large"
           show={this.state.show}
           onHide={this.onHide}
           >
@@ -195,18 +311,19 @@ export default React.createClass({
               {this.props.tableId}
               <SapiTableLink
                  tableId={this.props.tableId}>
-                  <small className="btn btn-link">
-                    Explore in Console
-                  </small>
+                <small className="btn btn-link">
+                  Explore in Console
+                </small>
               </SapiTableLink>
+              <RefreshIcon
+                 isLoading={this.state.isReloading}
+                 onClick={this.reload}
+              />
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
             {modalBody}
           </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.onHide}>Close</Button>
-          </Modal.Footer>
         </Modal>
       </div>
     );
@@ -214,6 +331,10 @@ export default React.createClass({
 
 
   renderTooltip(){
+    if (this.state.isLoading){
+      return (<span> Loading... </span>);
+    }
+
     const table = this.state.table;
     return (
       <span>
@@ -232,9 +353,21 @@ export default React.createClass({
 
   onHide(){
     this.setState({show: false});
+    this.stopEventService();
+  },
+
+  reload(){
+    this.setState({isReloading: true});
+    Promise.props( {
+      'loadAllTablesFore': storageActions.loadTablesForce(),
+      'exportData': this.exportDataSample(),
+      'loadEvents': this.state.eventService.load()
+    }).then( () => this.setState({isReloading: false}));
   },
 
   onShow(){
+    this.exportDataSample();
+    this.startEventService();
     this.setState({show: true});
   },
 
@@ -249,9 +382,62 @@ export default React.createClass({
       return row.get(columnIndex);
     });
     return result;
+  },
+
+  startEventService(){
+    this.state.eventService.addChangeListener(this.handleEventsChange);
+    this.state.eventService.load();
+  },
+
+  stopEventService(){
+    this.state.eventService.stopAutoReload();
+    this.state.eventService.removeChangeListener(this.handleEventsChange);
+  },
+
+  handleEventsChange(){
+    const events = this.state.eventService.getEvents();
+    this.setState({events: events});
+  },
+
+  exportDataSample(){
+    this.setState({
+      loadingPreview: true
+    });
+    const component = this;
+    return storageApi
+    .exportTable(this.props.tableId, {limit: 10})
+    .then( (csv) => {
+      component.setState({
+        loadingPreview: false,
+        dataPreview: csv
+      });
+    });
+
+  },
+
+
+  eventsTemplates: {
+    'storage.tableImportStarted': {
+      'message': 'Import started',
+      'className': ''
+    },
+
+    'storage.tableImportDone': {
+      'message': 'Successfully imported ',
+      'className': 'success'
+    },
+
+    'storage.tableImportError': {
+      'message': 'Error on table import',
+      'className': 'error'
+    },
+
+    'storage.tableExported': {
+      'message': 'Exported to a csv file',
+      'className': 'info'
+    }
+
   }
-
-
 
 
 });
