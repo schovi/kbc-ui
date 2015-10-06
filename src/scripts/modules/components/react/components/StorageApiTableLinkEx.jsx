a7ccbb674c043763708c21ead386c7f44cc9e7cd
Tomas Kacur <tomas.kacur@keboola.com>
2015-10-06 14:47:40 +0200

    refactor(SapiTableLinkEx)

--- src/scripts/modules/components/react/components/StorageApiTableLinkEx.jsx
+++ src/scripts/modules/components/react/components/StorageApiTableLinkEx.jsx
@@ -3,25 +3,16 @@ import Immutable, {Map} from 'immutable';
 import _ from 'underscore';
 import Promise from 'bluebird';
 import moment from 'moment';
-import string from 'underscore.string';
-
-import {Input, Table, Modal} from 'react-bootstrap';
-import {TabbedArea, TabPane} from 'react-bootstrap';
 import filesize from 'filesize';
-import {RefreshIcon} from 'kbc-react-components';
-
-import Tooltip from '../../../../react/common/Tooltip';
-import SapiTableLink from './StorageApiTableLink';
-import EmptyState from '../../../components/react/components/ComponentEmptyState';
-import date from '../../../../utils/date';
-
 
 import storageActions from '../../StorageActionCreators';
 import storageApi from '../../StorageApi';
-
 import tablesStore from '../../stores/StorageTablesStore';
-import createStoreMixin from '../../../../react/mixins/createStoreMixin';
 
+import TableLinkModalDialog from './StorageApiTableLinkExComponents/ModalDialog';
+import Tooltip from '../../../../react/common/Tooltip';
+
+import createStoreMixin from '../../../../react/mixins/createStoreMixin';
 import EventsService from '../../../sapi-events/EventService';
 
 export default React.createClass({
@@ -99,110 +90,53 @@ export default React.createClass({
 
   },
 
+  renderTooltip(){
+    if (this.state.isLoading){
+      return 'Loading';
+    }
 
-
-  renderEvents(){
+    const table = this.state.table;
     if (!this.tableExists()){
-      return (
-        <EmptyState>
-          No Data.
-        </EmptyState>
-      );
+      return 'Table does not exist yet.';
     }
-    //console.log(this.state.events.toJS(), this.state.events.count());
-    const events = this.state.events;
-    const rows = events.map( (e) => {
-      const event = e.get('event');
-      let info = this.eventsTemplates[event];
-      if (!info){
-        info = {
-          className: '',
-          message: e.get('message')
-        };
-      }
-      const cl = `tr ${info.className}`;
-      const agoTime = moment(e.get('created')).fromNow();
-      const incElement = (<p><small><strong>incremental</strong></small></p>);
-      info.message = string.replaceAll(info.message, this.props.tableId, '');
-      const incremental = e.getIn(['params', 'incremental']) ? incElement : (<span></span>);
-      return (
-        <tr className={cl}>
-          <td className="td">
-            {e.get('id')}
-          </td>
-          <td className="td">
-            {date.format(e.get('created'))}
-            <small> {agoTime} </small>
-          </td>
-          <td className="td">
-            {e.get('component')}
-          </td>
-          <td className="td">
-            {info.message}
-            {incremental}
-          </td>
-          <td className="td">
-            {e.getIn(['token', 'name'])}
-          </td>
-        </tr>
-      );
-    }
-    );
-
-
 
     return (
-      <span>
-        <Input>
-        <div className="col-xs-3">
-          <div className="checkbox">
-            <label>
-              <input
-                checked={this.state.omitFetches}
-                onClick={this.onOmitFetches}
-                type="checkbox"/> Ignore table fetches
-            </label>
-          </div>
+      <span key="tooltipinfo">
+        <div>
+          {moment(table.get('lastChangeDate')).fromNow()}
         </div>
-        <div className="col-xs-3">
-          <div className="checkbox">
-            <label>
-              <input
-                 checked={this.state.omitExports}
-                 onClick={this.onOmitExports}
-                 type="checkbox"/> Ignore table exports
-            </label>
-          </div>
+        <div>
+          {filesize(table.get('dataSizeBytes'))}
         </div>
-        </Input>
-
-        <table className="table table-striped">
-          <thead className="thead">
-            <tr className="tr">
-              <th className="th">
-                Id
-              </th>
-              <th className="th">
-                Created
-              </th>
-              <th className="th">
-                Component
-              </th>
-              <th className="th">
-                Event
-              </th>
-              <th className="th">
-                Creator
-              </th>
-
-            </tr>
-          </thead>
-          <tbody className="tbody">
-            {rows}
-          </tbody>
-        </table>
-      </span>);
+        <div>
+          {table.get('rowsCount')} rows
+        </div>
+      </span>
+    );
   },
+
+  renderModal(){
+    return (
+      <TableLinkModalDialog
+         show={this.state.show}
+         tableId={this.props.tableId}
+         reload={this.reload}
+         tableExists={this.tableExists()}
+         omitFetches={this.state.omitFetches}
+         omitExports={this.state.omitExports}
+         onHideFn={this.onHide}
+         isLoading={this.isLoading()}
+         table={this.state.table}
+         dataPreview={this.state.dataPreview}
+         onOmitExportsFn={this.onOmitExports}
+         onOmitFetchesFn={this.onOmitFetches}
+         events={this.state.events}
+      />
+
+    );
+
+  },
+
   onOmitExports(e){
     const checked = e.target.checked;
     this.setState({omitExports: checked});
@@ -232,198 +166,9 @@ export default React.createClass({
 
   },
 
-
-  renderDataSample(){
-    const data = this.state.dataPreview;
-    if (data.count() === 0){
-      return (
-        <EmptyState>
-          No Data.
-        </EmptyState>
-      );
-    }
-
-    const header = data.first().map( (c) => {
-      return (
-        <th>
-          {c}
-        </th>
-      );
-    }).toArray();
-    const rows = data.rest().map( (row) => {
-      const cols = row.map( (c) => {
-        return (<td> {c} </td>);
-      });
-
-      return (
-        <tr>
-          {cols}
-        </tr>);
-    });
-
-    return (
-      <div>
-        <Table responsive className="table table-striped">
-          <thead>
-            <tr>
-              {header}
-            </tr>
-          </thead>
-          <tbody>
-            {rows}
-          </tbody>
-        </Table>
-      </div>
-    );
-  },
-
-  renderColumnHeader(column){
-    const {table} = this.state,
-    indexed = table.get('indexedColumns'),
-    primary = table.get('primaryKey');
-    return (
-      <span>
-        {column}
-        <div>
-          {indexed.indexOf(column) > -1 ? ( <small><span className="label label-info">index</span></small>) : '' }
-          {primary.indexOf(column) > -1 ? ( <small><span className="label label-info">PK</span></small>) : '' }
-        </div>
-      </span>
-    );
-
-
-  },
-
-  renderColumnsInfo(){
-    if (!this.tableExists() || !this.isDataPreview()){
-      return (
-        <EmptyState>
-          No Data.
-        </EmptyState>
-      );
-    }
-    const {table} = this.state;
-    const columns = table.get('columns');
-
-    const columnsRows = columns.map((c) => {
-      const values = this.getColumnValues(c);
-      let result = values.filter((val) => val !== '').join(', ');
-      return this.renderTableRow(this.renderColumnHeader(c), result);
-    });
-
-    return (
-      <div>
-        <Table responsive className="table table-striped">
-          <thead>
-            <tr>
-              <th>
-                Column
-              </th>
-              <th>
-                Sample Values
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {columnsRows}
-          </tbody>
-        </Table>
-      </div>
-    );
-  },
-
-  renderGeneralInfo(){
-    if (!this.tableExists())
-    {
-      let msg = 'Table does not exist yet.';
-      if (this.state.isLoading){
-        msg = 'Loading...';
-      }
-      return (
-        <EmptyState key="emptytable">
-          {msg}
-        </EmptyState>
-      );
-
-    }
-    const table = this.state.table;
-    const primaryKey = table.get('primaryKey').toJS();
-    const indexes = table.get('indexedColumns').toJS();
-    return (
-      <div>
-        <Table responsive className="table">
-          <thead>
-            <tr>
-              <td style={{width: '20%'}}>
-                ID
-              </td>
-              <td>
-                {table.get('id')}
-              </td>
-            </tr>
-          </thead>
-          <tbody>
-            {this.renderTableRow('Storage', table.get('bucket').get('backend'))}
-            {this.renderTableRow('Created', this.renderTimefromNow(table.get('created')))}
-            {this.renderTableRow('Primary Key', _.isEmpty(primaryKey) ? 'N/A' : primaryKey.join(', '))}
-            {this.renderTableRow('Last Import', this.renderTimefromNow(table.get('lastImportDate')))}
-            {this.renderTableRow('Last Change', this.renderTimefromNow(table.get('lastChangeDate')))}
-
-            {this.renderTableRow('Rows Count', table.get('rowsCount') + ' rows')}
-            {this.renderTableRow('Data Size', filesize(table.get('dataSizeBytes')))}
-            {this.renderTableRow('Indexed Column(s)', _.isEmpty(indexes) ? 'N/A' : indexes.join(', '))}
-            {this.renderTableRow('Columns', table.get('columns').count() + ' columns: ' + table.get('columns').join(', '))}
-          </tbody>
-        </Table>
-      </div>
-    );
-  },
-
-  renderTableRow(name, value){
-    return (
-      <tr>
-        <td>
-          {name}
-        </td>
-        <td>
-          {value}
-        </td>
-      </tr>
-    );
-
-  },
-
-  renderModal(){
-  },
-
   isLoading(){
     return this.state.isLoading || this.state.loadingPreview || this.state.eventService.getIsLoading();
 
-  },
-
-  renderTooltip(){
-    if (this.state.isLoading){
-      return 'Loading';
-    }
-
-    const table = this.state.table;
-    if (!this.tableExists()){
-      return 'Table does not exist yet.';
-    }
-
-    return (
-      <span key="tooltipinfo">
-        <div>
-          {moment(table.get('lastChangeDate')).fromNow()}
-        </div>
-        <div>
-          {filesize(table.get('dataSizeBytes'))}
-        </div>
-        <div>
-          {table.get('rowsCount')} rows
-        </div>
-      </span>
-    );
   },
 
   onHide(){
@@ -448,17 +193,6 @@ export default React.createClass({
   },
 
 
-  getColumnValues(columnName){
-    const data = this.state.dataPreview;
-    const columnIndex = data.first().indexOf(columnName);
-
-    const result = data
-    .shift()
-    .map( (row) => {
-      return row.get(columnIndex);
-    });
-    return result;
-  },
 
   startEventService(){
     this.state.eventService.addChangeListener(this.handleEventsChange);
@@ -498,44 +232,7 @@ export default React.createClass({
 
   tableExists(){
     return !_.isEmpty(this.state.table.toJS());
-  },
-
-  isDataPreview(){
-    return !_.isEmpty(this.state.dataPreview.toJS());
-  },
-
-  eventsTemplates: {
-    'storage.tableImportStarted': {
-      'message': 'Import started',
-      'className': ''
-    },
-
-    'storage.tableImportDone': {
-      'message': 'Successfully imported ',
-      'className': 'success'
-    },
-
-    'storage.tableImportError': {
-      'message': 'Error on table import',
-      'className': 'error'
-    },
-
-    'storage.tableExported': {
-      'message': 'Exported to a csv file',
-      'className': 'info'
-    }
-
-  },
-
-  renderTimefromNow(value){
-
-    const fromNow = moment(value).fromNow();
-    return (
-      <span> {date.format(value)}
-        <small> {fromNow} </small>
-      </span>
-    );
-
   }
+
 
 });
