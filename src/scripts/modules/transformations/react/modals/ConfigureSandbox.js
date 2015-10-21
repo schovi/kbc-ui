8f9e11afb04e2c954b93cab50ded1b0244227312
Martin Halamíček <martin@halamicek.cz>
2015-10-21 16:28:35 +0200

    feat(transformations): transformation sandbox don't redirect to job FIXES #326

---
+++ src/scripts/modules/transformations/react/modals/ConfigureSandbox.js
@@ -0,0 +1,122 @@
+import React, {PropTypes} from 'react';
+import ConfigureSandboxModal from './ConfigureSandboxModal';
+import createStoreMixin from '../../../../react/mixins/createStoreMixin';
+import MySqlSandboxCredentialsStore from '../../../provisioning/stores/MySqlSandboxCredentialsStore';
+import jobsApi from '../../../jobs/JobsApi';
+import actionCreators from '../../../components/InstalledComponentsActionCreators';
+
+
+export default React.createClass({
+  propTypes: {
+    show: PropTypes.bool.isRequired,
+    onHide: PropTypes.func.isRequired,
+    defaultMode: PropTypes.string.isRequired,
+    backend: PropTypes.string.isRequired,
+    runParams: PropTypes.object.isRequired
+  },
+  mixins: [createStoreMixin(MySqlSandboxCredentialsStore)],
+
+  getStateFromStores() {
+    return {
+      mysqlCredentials: MySqlSandboxCredentialsStore.getCredentials()
+    };
+  },
+
+  getInitialState() {
+    return {
+      mode: this.props.defaultMode,
+      isRunning: false,
+      jobId: null,
+      progress: null,
+      progressStatus: null
+    };
+  },
+
+  render() {
+    return React.createElement(ConfigureSandboxModal, {
+      mysqlCredentials: this.state.mysqlCredentials,
+      onHide: this.handleModalClose,
+      show: this.props.show,
+      backend: this.props.backend,
+      mode: this.state.mode,
+      jobId: this.state.jobId,
+      progress: this.state.progress,
+      progressStatus: this.state.progressStatus,
+      isRunning: this.state.isRunning,
+      onModeChange: this.handleModeChange,
+      onCreateStart: this.handleSandboxCreate
+    });
+  },
+
+  handleModeChange(e) {
+    this.setState({
+      mode: e.target.value
+    });
+  },
+
+  handleSandboxCreate() {
+    this.setState({
+      isRunning: true,
+      jobId: null,
+      progress: 'Waiting for load start ...',
+      progressStatus: null
+    });
+    actionCreators.runComponent({
+      component: 'transformation',
+      notify: false,
+      data: this.props.runParams.set('mode', this.state.mode).toJS()
+    }).then(this.handleJobReceive).catch((e) => {
+      this.setState({
+        isRunning: false
+      });
+      throw e;
+    });
+  },
+
+  handleJobReceive(job) {
+    if (!this.isMounted()) {
+      return;
+    }
+    if (job.isFinished) {
+      if (job.status === 'success') {
+        this.setState({
+          isRunning: false,
+          progress: 'Sandbox is successfully loaded. You can start using it now!',
+          progressStatus: 'success',
+          jobId: null
+        });
+      } else {
+        this.setState({
+          isRunning: false,
+          progress: 'Load finished with error.',
+          progressStatus: 'danger',
+          jobId: job.id
+        });
+      }
+    } else {
+      this.setState({
+        jobId: job.id,
+        progress: job.state === 'waiting' ? 'Waiting for load start ...' : 'Loading data into sandbox ...'
+      });
+      setTimeout(this.checkJobStatus, 5000);
+    }
+  },
+
+  checkJobStatus() {
+    jobsApi
+      .getJobDetail(this.state.jobId)
+      .then(this.handleJobReceive)
+      .catch((e) => {
+        this.setState({
+          isRunning: false
+        });
+        throw e;
+      });
+  },
+
+  handleModalClose() {
+    // reset state
+    this.setState(this.getInitialState());
+    this.props.onHide();
+  }
+});
---
+++ src/scripts/modules/transformations/react/modals/ConfigureSandboxModal.jsx
@@ -0,0 +1,118 @@
+import React, {PropTypes} from 'react';
+import {Modal, Input} from 'react-bootstrap';
+import {Link} from 'react-router';
+import RadioGroup from 'react-radio-group';
+import MySqlCredentialsContainer from '../components/MySqlCredentialsContainer';
+import RedshiftCredentialsContainer from '../components/RedshiftCredentialsContainer';
+import ConnectToMySqlSandbox from '../components/ConnectToMySqlSandbox';
+import ConfirmButtons from '../../../../react/common/ConfirmButtons';
+
+function renderStatusIcon(status) {
+  switch(status) {
+    case 'success':
+      return <span className="fa fa-check"/>;
+    case 'danger':
+      return <span className="fa fa-times"/>;
+    default:
+      return null;
+  }
+}
+
+export default React.createClass({
+  propTypes: {
+    show: PropTypes.bool.isRequired,
+    onHide: PropTypes.func.isRequired,
+    mode: PropTypes.string.isRequired,
+    backend: PropTypes.string.isRequired,
+    progress: PropTypes.string.isRequired,
+    progressStatus: PropTypes.string.isRequired,
+    isRunning: PropTypes.bool,
+    jobId: PropTypes.string.isRequired,
+    mysqlCredentials: PropTypes.object.isRequired,
+    onModeChange: PropTypes.func.isRequired,
+    onCreateStart: PropTypes.func.isRequired
+  },
+
+  render() {
+    return (
+      <Modal show={this.props.show} onHide={this.props.onHide}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create Sandbox</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <h3>Mode</h3>
+            <RadioGroup name="mode" value={this.props.mode} onChange={this.props.onModeChange}>
+              <div className="form-horizontal">
+                <Input type="radio" label="Load input tables only" wrapperClassName="col-sm-offset-1 col-sm-8" value="input" />
+                <Input type="radio" label="Prepare transformation" help="Load input tables AND perform required transformations" wrapperClassName="col-sm-offset-1 col-sm-8" value="prepare" />
+                <Input type="radio" label="Execute transformation without writing to Storage API" wrapperClassName="col-sm-offset-1 col-sm-8" value="dry-run" />
+              </div>
+            </RadioGroup>
+          </div>
+          {this.renderCredentials()}
+        </Modal.Body>
+        <Modal.Footer>
+          <div className="pull-left" style={{padding: '6px 12px'}}>
+            <span className={'text-' + this.props.progressStatus}>
+              {renderStatusIcon(this.props.progressStatus)} {this.props.progress}
+              {this.props.jobId ? <Link to="jobDetail" params={{jobId: this.props.jobId}} > More details</Link> : null}
+            </span>
+          </div>
+          <ConfirmButtons
+            onCancel={this.props.onHide}
+            onSave={this.props.onCreateStart}
+            saveLabel="Create"
+            isSaving={this.props.isRunning}
+            />
+        </Modal.Footer>
+      </Modal>
+    );
+  },
+
+  renderCredentials() {
+    if (this.props.backend === 'mysql' || this.props.backend === 'redshift') {
+      return (
+        <div className="clearfix">
+          <h3>Credentials</h3>
+          <div className="col-sm-offset-1 col-sm-10">
+            {this.props.backend === 'redshift' ? this.renderRedshiftCredentials() : this.renderMysqlCredentials()}
+          </div>
+        </div>
+      );
+    }
+  },
+
+  renderRedshiftCredentials() {
+    return React.createElement(RedshiftCredentialsContainer, {
+      isAutoLoad: true
+    });
+  },
+
+  renderMysqlCredentials() {
+    return (
+      <div className="row">
+        <div className="col-md-9">
+          <MySqlCredentialsContainer isAutoLoad={true} />
+        </div>
+        <div className="col-md-3">
+          {this.renderMysqlConnect()}
+        </div>
+      </div>
+    );
+  },
+
+  renderMysqlConnect() {
+    if (!this.props.mysqlCredentials.get('id')) {
+      return null;
+    }
+    return (
+      <ConnectToMySqlSandbox credentials={this.props.mysqlCredentials}>
+        <button className="btn btn-link" title="Connect to Sandbox" type="submit">
+          <span className="fa fa-fw fa-database"/> Connect
+        </button>
+      </ConnectToMySqlSandbox>
+    );
+  }
+
+});
