56869b7d449e0705390cc8ff516281750504163c
Martin Halamíček <martin@halamicek.cz>
2015-11-23 15:40:56 +0100

    fix(ex-dropbox): oauth redirect don't update config

--- src/scripts/modules/ex-dropbox/routes.js
+++ src/scripts/modules/ex-dropbox/routes.js
@@ -1,10 +1,8 @@
 import Index from './react/Index';
 import installedComponentsActions from '../components/InstalledComponentsActionCreators';
-import installedComponentsStore from '../components/stores/InstalledComponentsStore';
 import jobsActionCreators from '../jobs/ActionCreators';
 import oauthStore from '../components/stores/OAuthStore';
 import oauthActions from '../components/OAuthActionCreators';
-import Immutable from 'immutable';
 import RouterStore from '../../stores/RoutesStore';
 import ApplicationActionCreators from '../../actions/ApplicationActionCreators';
 
@@ -30,22 +28,14 @@ export default {
       title: '',
       requireData: [
         (params) => installedComponentsActions.loadComponentConfigData('ex-dropbox', params.config).then(() => {
-          let configuration = installedComponentsStore.getConfigData('ex-dropbox', params.config).toJS();
           return oauthActions.loadCredentials('ex-dropbox', params.config).then(() => {
-            let credentials = oauthStore.getCredentials('ex-dropbox', params.config).toJS();
-            let parameters = configuration ? configuration.parameters : {};
-            parameters.credentials = params.config;
-            configuration.parameters = parameters;
-            let description = credentials ? credentials.description : '';
-            let saveFn = installedComponentsActions.saveComponentConfigData;
-            return saveFn('ex-dropbox', params.config, Immutable.fromJS(configuration)).then(() => {
-              let router = RouterStore.getRouter();
-              let notification = `Dropbox account ${description} successfully authorized.`;
-              ApplicationActionCreators.sendNotification({
-                message: notification
-              });
-              return router.transitionTo('ex-dropbox', {config: params.config});
+            let credentials = oauthStore.getCredentials('wr-dropbox', params.config);
+            let notification = `Dropbox account ${credentials.get('description')} successfully authorized.`;
+            let router = RouterStore.getRouter();
+            ApplicationActionCreators.sendNotification({
+              message: notification
             });
+            return router.transitionTo('ex-dropbox', {config: params.config});
           }, (error) => {
             let router = RouterStore.getRouter();
             let notification = `Failed to authorize the Dropbox account, error: ${error}, please contact us on support@keboola.com`;
