193b5c32ec4b7bf217c42e38550210566795b8a7
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2015-11-25 18:22:48 +0800

    fix(ComponentIcon): show 3rd party label instead of icon

--- src/scripts/react/common/ComponentIcon.jsx
+++ src/scripts/react/common/ComponentIcon.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Label} from 'react-bootstrap';
 import classNames from 'classnames';
 
 import ApplicationStore from '../../stores/ApplicationStore';
@@ -6,7 +7,6 @@ import ApplicationStore from '../../stores/ApplicationStore';
 import extractor from '../../../images/extractor-32-1.png';
 import writer from '../../../images/writer-32-1.png';
 import other from '../../../images/other-32-1.png';
-import ComponentDetailLink from './ComponentDetailLink.coffee';
 
 const DEFAULT_ICON_IMAGES = {
   extractor: extractor,
@@ -37,25 +37,18 @@ export default React.createClass({
     if (!component) {
       return false;
     }
-    return !!this.props.component.get('flags').contains('3rdParty');
+    return !!component.get('flags').contains('3rdParty');
   },
 
-  get3rdPartyIcon() {
+  get3rdPartyLabel() {
     if (this.is3rdParty()) {
-      if (this.props.size === '32') {
-        return (
-          <ComponentDetailLink type={ this.props.component.get('type') } componentId={ this.props.component.get('id') }>
-            <i className="fa fa-cloud kbcComponentIcon-3rdParty-32px" title="3rd party application"></i>
-          </ComponentDetailLink>
-        );
-      }
-      if (this.props.size === '64') {
-        return (
-          <ComponentDetailLink type={ this.props.component.get('type') } componentId={ this.props.component.get('id') }>
-            <i className="fa fa-cloud kbcComponentIcon-3rdParty-64px" title="3rd party application"></i>
-          </ComponentDetailLink>
-        );
-      }
+      return (
+        <small className="thirdPartyLabel">
+          <Label title="3rd party application">
+            3rd
+          </Label>
+        </small>
+      );
     } else {
       return null;
     }
@@ -81,7 +74,7 @@ export default React.createClass({
     return (
       <span className={classNames('kb-sapi-component-icon', this.props.className)}>
         <img src={ url } />
-        { this.get3rdPartyIcon() }
+        { this.get3rdPartyLabel() }
       </span>
     );
   }
