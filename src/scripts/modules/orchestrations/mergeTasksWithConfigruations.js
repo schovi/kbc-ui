5ca5e4c24df382062ef0f7c99b3dc35833a03b7f
Martin Halamíček <martin@halamicek.cz>
2015-11-27 14:52:33 +0100

    fix(orchestrations): config for task fallback for old account param

--- src/scripts/modules/orchestrations/mergeTasksWithConfigruations.js
+++ src/scripts/modules/orchestrations/mergeTasksWithConfigruations.js
@@ -1,6 +1,9 @@
 export default function(tasks, configurations) {
   return tasks.map((task) => {
-    const configId = task.getIn(['actionParameters', 'config'], task.getIn(['actionParameters', 'configBucketId'])),
+    const configId = task.getIn(['actionParameters', 'config'],
+        task.getIn(['actionParameters', 'configBucketId'],
+          task.getIn(['actionParameters', 'account'])
+        )),
       componentId = task.get('component'),
       config = configurations.getIn([componentId, 'configurations', configId]);
 
