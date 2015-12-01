301fe124c6b6b9fd589b4ad3f06423d88cfe5bc8
Martin Halamíček <martin@halamicek.cz>
2015-12-01 14:59:47 +0100

    fix(GenericDockerDetail): parameters header FIXES #429

--- src/scripts/modules/components/react/pages/GenericDockerDetail.jsx
+++ src/scripts/modules/components/react/pages/GenericDockerDetail.jsx
@@ -184,6 +184,7 @@ export default React.createClass({
                 onEditChange={this.onEditParametersChange}
                 onEditSubmit={this.onEditParametersSubmit}
                 isValid={this.state.isValidEditingConfigDataParameters}
+                headerText="Parameters"
                 />
               {this.runtimeConfiguration()}
             </div>
