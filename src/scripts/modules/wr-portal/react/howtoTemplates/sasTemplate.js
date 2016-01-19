81d397550674644a45fa382e9803fa0f9f23c164
Tomas Kacur <tomas.kacur@keboola.com>
2016-01-19 17:32:29 +0100

    fix(wr-portal): help style fix

--- src/scripts/modules/wr-portal/react/howtoTemplates/sasTemplate.js
+++ src/scripts/modules/wr-portal/react/howtoTemplates/sasTemplate.js
@@ -6,10 +6,9 @@ const sasTemplate = `
 2. #### Download ODBC redshift driver from the following link:
    <a href="http://docs.aws.amazon.com/redshift/latest/mgmt/configure-odbc-connection.html" target="_blank"> http://docs.aws.amazon.com/redshift/latest/mgmt/configure-odbc-connection.html </a>
 3. #### In SAS initiate the connection  from \`ETL Enterprise Guide\`, sample command:
-
-   \`\`\`
-    libname RedShift odbc datasrc="RedShift" schema="out.c-sas-va" PRESERVE_COL_NAMES =YES PRESERVE_TAB_NAMES=YES;
-   \`\`\`
+\`\`\`
+libname RedShift odbc datasrc="RedShift" schema="out.c-sas-va" PRESERVE_COL_NAMES =YES PRESERVE_TAB_NAMES=YES;
+\`\`\`
 
 * If you already have other ODBC driver than Redshift licensed, you can configure a keboola database writer for it and connect to its database.
 
