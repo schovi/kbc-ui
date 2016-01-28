65d5917f5bf3b79152a5023f165bf3dd6e11b035
Martin Halamíček <martin@halamicek.cz>
2016-01-28 14:10:53 +0100

    fix(orhcestrations): edit tasks error 2

--- src/scripts/modules/orchestrations/mergeTasksWithConfigruations.js
+++ src/scripts/modules/orchestrations/mergeTasksWithConfigruations.js
@@ -2,8 +2,8 @@ export default function(tasks, configurations) {
   return tasks.map((task) => {
     const configId = task.getIn(['actionParameters', 'config'],
         task.getIn(['actionParameters', 'configBucketId'],
-          task.getIn(['actionParameters', 'account'])
-        ), ''),
+          task.getIn(['actionParameters', 'account'], '')
+        )),
       componentId = task.get('component'),
       config = configurations.getIn([componentId, 'configurations', configId.toString()]);
 
