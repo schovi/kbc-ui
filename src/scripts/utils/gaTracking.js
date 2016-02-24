af9acab92c818a75309b1e9cae9dbda398802606
Martin Halamíček <martin@halamicek.cz>
2016-02-24 12:57:52 +0100

    google analytics tracking - don't modify project url

--- src/scripts/utils/gaTracking.js
+++ src/scripts/utils/gaTracking.js
@@ -1,10 +1,5 @@
 
-
-export function modifyProjectPageUrl(pathname) {
-  return pathname.replace(/^(\/admin\/projects\/)([0-9]+)/, '$1project');
-}
-
 export function sendPageView() {
   /* global ga */
-  ga('send', 'pageview', modifyProjectPageUrl(window.location.pathname));
+  ga('send', 'pageview', window.location.pathname);
 }
