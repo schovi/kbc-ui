b027841b126f29bbb4b3b4acf7e40258aa1a9b76
Martin Halamíček <martin@halamicek.cz>
2016-04-07 09:38:18 +0200

    ex-twitter - configuration template v2

--- src/scripts/modules/ex-twitter/template.js
+++ src/scripts/modules/ex-twitter/template.js
@@ -34,6 +34,228 @@ const common = {
   }
 };
 
+const tweetDataMapping = {
+  'id': {
+    'mapping': {
+      'destination': 'id',
+      'primaryKey': true
+    }
+  },
+  'entities.hashtags': {
+    'type': 'table',
+    'destination': 'tweets-hashtags',
+    'tableMapping': {
+      'text': {
+        'mapping': {
+          'destination': 'text',
+          'primaryKey': true
+        }
+      }
+    },
+    'parentKey': {
+      'primaryKey': true
+    }
+  },
+  'entities.user_mentions': {
+    'type': 'table',
+    'destination': 'tweets-user-mentions',
+    'tableMapping': {
+      'name': {
+        'mapping': {
+          'destination': 'name'
+        }
+      },
+      'screen_name': {
+        'mapping': {
+          'destination': 'screen_name'
+        }
+      },
+      'id': {
+        'mapping': {
+          'destination': 'user_id',
+          'primaryKey': true
+        }
+      }
+    },
+    'parentKey': {
+      'primaryKey': true
+    }
+  },
+  'entities.urls': {
+    'type': 'table',
+    'destination': 'tweets-urls',
+    'tableMapping': {
+      'url': {
+        'mapping': {
+          'destination': 'url',
+          'primaryKey': true
+        }
+      },
+      'expanded_url': {
+        'mapping': {
+          'destination': 'expanded_url'
+        }
+      },
+      'display_url': {
+        'mapping': {
+          'destination': 'display_url'
+        }
+      }
+    },
+    'parentKey': {
+      'primaryKey': true
+    }
+  },
+  'created_at': {
+    'mapping': {
+      'destination': 'created_at'
+    }
+  },
+  'favorite_count': {
+    'mapping': {
+      'destination': 'favorite_count'
+    }
+  },
+  'in_reply_to_screen_name': {
+    'mapping': {
+      'destination': 'in_reply_to_screen_name'
+    }
+  },
+  'in_reply_to_status_id': {
+    'mapping': {
+      'destination': 'in_reply_to_status_id'
+    }
+  },
+  'in_reply_to_user_id': {
+    'mapping': {
+      'destination': 'in_reply_to_user_id'
+    }
+  },
+  'lang': {
+    'mapping': {
+      'destination': 'lang'
+    }
+  },
+  'quoted_status_id': {
+    'mapping': {
+      'destination': 'quoted_status_id'
+    }
+  },
+  'retweet_count': {
+    'mapping': {
+      'destination': 'retweet_count'
+    }
+  },
+  "retweeted_status.id": {
+    "mapping": {
+      "destination": "retweeted_status_id"
+    }
+  },
+  "retweeted_status": {
+    "type": "table",
+    "destination": "tweets",
+    "parentKey": {
+      "disable": true
+    }
+  },
+  'source': {
+    'mapping': {
+      'destination': 'source'
+    }
+  },
+  'text': {
+    'mapping': {
+      'destination': 'text'
+    }
+  },
+  'truncated': {
+    'mapping': {
+      'destination': 'truncated'
+    }
+  },
+  'withheld_copyright': {
+    'mapping': {
+      'destination': 'withheld_copyright'
+    }
+  },
+  'user.id': {
+    'mapping': {
+      'destination': 'user_id'
+    }
+  },
+  "user": {
+    "type": "table",
+    "destination": "users",
+    "tableMapping": {
+      "id": {
+        "mapping": {
+          "destination": "id",
+          "primaryKey": true
+        }
+      },
+      "name": {
+        "mapping": {
+          "destination": "name"
+        }
+      },
+      "screen_name": {
+        "mapping": {
+          "destination": "screen_name"
+        }
+      },
+      "created_at": {
+        "mapping": {
+          "destination": "created_at"
+        }
+      },
+      "description": {
+        "mapping": {
+          "destination": "description"
+        }
+      },
+      "favourites_count": {
+        "mapping": {
+          "destination": "favourites_count"
+        }
+      },
+      "followers_count": {
+        "mapping": {
+          "destination": "followers_count"
+        }
+      },
+      "friends_count": {
+        "mapping": {
+          "destination": "friends_count"
+        }
+      },
+      "lang": {
+        "mapping": {
+          "destination": "lang"
+        }
+      },
+      "location": {
+        "mapping": {
+          "destination": "location"
+        }
+      },
+      "statuses_count": {
+        "mapping": {
+          "destination": "statuses_count"
+        }
+      }
+    },
+    "parentKey": {
+      "disable": true
+    }
+  },
+  'keboola_source': {
+    'type': 'user',
+    'mapping': {
+      'destination': 'keboola_source'
+    }
+  }
+};
+
 const searchTemplate = {
   'endpoint': 'search/tweets.json',
   'dataType': 'tweets',
@@ -43,208 +265,41 @@ const searchTemplate = {
   },
   'scroller': 'cursor_timeline',
   'userData': {
-    'q': 'pinkbike',
     'keboola_source': 'search'
   },
-  'dataMapping': {
-    'id': {
-      'mapping': {
-        'destination': 'id',
-        'primaryKey': true
-      }
-    },
-    'entities.hashtags': {
-      'type': 'table',
-      'destination': 'tweets-hashtags',
-      'tableMapping': {
-        'text': {
-          'mapping': {
-            'destination': 'text',
-            'primaryKey': true
-          }
-        }
-      },
-      'parentKey': {
-        'primaryKey': true
-      }
-    },
-    'entities.user_mentions': {
-      'type': 'table',
-      'destination': 'tweets-user-mentions',
-      'tableMapping': {
-        'name': {
-          'mapping': {
-            'destination': 'name'
-          }
-        },
-        'screen_name': {
-          'mapping': {
-            'destination': 'screen_name'
-          }
-        },
-        'id': {
-          'mapping': {
-            'destination': 'user_id',
-            'primaryKey': true
-          }
-        }
-      },
-      'parentKey': {
-        'primaryKey': true
-      }
-    },
-    'entities.urls': {
-      'type': 'table',
-      'destination': 'tweets-urls',
-      'tableMapping': {
-        'url': {
-          'mapping': {
-            'destination': 'url',
-            'primaryKey': true
-          }
-        },
-        'expanded_url': {
-          'mapping': {
-            'destination': 'expanded_url'
-          }
-        },
-        'display_url': {
-          'mapping': {
-            'destination': 'display_url'
-          }
-        }
-      },
-      'parentKey': {
-        'primaryKey': true
-      }
-    },
-    'created_at': {
-      'mapping': {
-        'destination': 'created_at'
-      }
-    },
-    'favorite_count': {
-      'mapping': {
-        'destination': 'favorite_count'
-      }
-    },
-    'in_reply_to_screen_name': {
-      'mapping': {
-        'destination': 'in_reply_to_screen_name'
-      }
-    },
-    'in_reply_to_status_id': {
-      'mapping': {
-        'destination': 'in_reply_to_status_id'
-      }
-    },
-    'in_reply_to_user_id': {
-      'mapping': {
-        'destination': 'in_reply_to_user_id'
-      }
-    },
-    'lang': {
-      'mapping': {
-        'destination': 'lang'
-      }
-    },
-    'quoted_status_id': {
-      'mapping': {
-        'destination': 'quoted_status_id'
-      }
-    },
-    'retweet_count': {
-      'mapping': {
-        'destination': 'retweet_count'
-      }
-    },
-    'source': {
-      'mapping': {
-        'destination': 'source'
-      }
-    },
-    'text': {
-      'mapping': {
-        'destination': 'text'
-      }
-    },
-    'truncated': {
-      'mapping': {
-        'destination': 'truncated'
-      }
-    },
-    'withheld_copyright': {
-      'mapping': {
-        'destination': 'withheld_copyright'
-      }
-    },
-    'user.id': {
-      'mapping': {
-        'destination': 'user_id'
-      }
-    },
-    'keboola_source': {
-      'type': 'user',
-      'mapping': {
-        'destination': 'keboola_source'
-      }
-    }
-  }
+  'dataMapping': tweetDataMapping
 };
 
 const userTimelineTemplate = {
-  'endpoint': 'statuses\/user_timeline.json',
+  'endpoint': 'statuses/user_timeline.json',
   'scroller': 'cursor_timeline',
-  'responseFilter': [
-    'place.bounding_box.coordinates',
-    'entities.media',
-    'entities.user_mentions[].indices',
-    'entities.hashtags[].indices',
-    'entities.urls[].indices',
-    'entities.symbols',
-    'geo',
-    'coordinates',
-    'extended_entities.media',
-    'quoted_status',
-    'retweeted_status'
-  ],
   'dataType': 'tweets',
-  'userData': {'api_source': 'user_timeline'},
+  'userData': {
+    'keboola_source': 'userTimeline'
+  },
   'params': {
-    'screen_name': '',
-    'trim_user': true,
-    'include_rts': false
-  }
+    'screen_name': ''
+  },
+  'dataMapping': tweetDataMapping
 };
 
 
-/**
+
 const mentionsTemplate = {
   'endpoint': 'statuses\/mentions_timeline.json',
   'scroller': 'cursor_timeline',
-  'responseFilter': [
-    'place.bounding_box.coordinates',
-    'entities.media',
-    'entities.user_mentions[].indices',
-    'entities.hashtags[].indices',
-    'entities.urls[].indices',
-    'entities.symbols',
-    'geo',
-    'coordinates',
-    'extended_entities.media',
-    'quoted_status',
-    'retweeted_status'
-  ],
   'dataType': 'tweets',
-  'userData': {'api_source': 'mentions_timeline'},
+  'userData': {
+    'keboola_source': 'mentions'
+  },
   'params': {
-    'include_entities': false,
+    'include_entities': true,
     'contributor_details': false,
     'trim_user': true,
     'include_rts': false
   }
 };
- **/
+
 
 const followersTemplate = {
   'endpoint': 'followers\/list.json',
@@ -276,7 +331,7 @@ export function createConfigurationFromSettings(settings) {
     jobs = jobs.push(fromJS(followersTemplate).setIn(['params', 'screen_name'], settings.get('followersScreenName')));
   }
 
-  // jobs = jobs.push(fromJS(mentionsTemplate));
+  jobs = jobs.push(fromJS(mentionsTemplate));
   return fromJS(common).setIn(['config', 'jobs'], jobs);
 }
 
