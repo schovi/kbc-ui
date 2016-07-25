64d1de8da8a5c2e2a6a3e32a80d1d937cf5db035
Martin Halamíček <martin@halamicek.cz>
2016-07-25 12:57:00 +0200

    billing - code style issues

--- src/scripts/modules/billing/Index.jsx
+++ src/scripts/modules/billing/Index.jsx
@@ -5,188 +5,187 @@ import FileSize from '../../react/common/FileSize';
 import ApplicationStore from '../../stores/ApplicationStore';
 
 function daySum(components, propertyName) {
-    return components
-        .filter(function(component) {
-            return component.has(propertyName);
-        })
-        .reduce(function(reduction, component) {
-            return reduction + component.get(propertyName);
-        }, 0);
+  return components
+    .filter(function(component) {
+      return component.has(propertyName);
+    })
+    .reduce(function(reduction, component) {
+      return reduction + component.get(propertyName);
+    }, 0);
 }
 
 const data = fromJS([
-    {
-        "date": "Jul 1",
-        "components": [
-            {
-                "name": "Google Analytics",
-                "containerOut": 2345,
-                "containerIn": 234324,
-                "storageIn": 43453,
-                "storageOut": 0
-            },
-            {
-                "name": "GoodData Writer",
-                "containerOut": 2345,
-                "containerIn": 234324,
-                "storageIn": 0,
-                "storageOut": 23424
-            },
-            {
-                "name": "Transformation",
-                "containerOut": 0,
-                "containerIn": 0,
-                "storageIn": 0,
-                "storageOut": 23424
-            },
-            {
-                "name": "Geneea",
-                "containerOut": 12334,
-                "containerIn": 12313,
-                "storageIn": 234324,
-                "storageOut": 23424,
-                "appUsage": 234
-            },
-            {
-                "name": "Storage Direct",
-                "storageIn": 123,
-                "storageOut": 23424
-            }
-        ]
-    },
-    {
-        "date": "Jul 2",
-        "components": [
-            {
-                "name": "Google Analytics",
-                "containerOut": 2345,
-                "containerIn": 234324,
-                "storageIn": 43453,
-                "storageOut": 0
-            },
-            {
-                "name": "GoodData Writer",
-                "containerOut": 2345,
-                "containerIn": 234324,
-                "storageIn": 0,
-                "storageOut": 23424
-            },
-            {
-                "name": "Transformation",
-                "containerOut": 0,
-                "containerIn": 0,
-                "storageIn": 0,
-                "storageOut": 23424
-            },
-            {
-                "name": "Storage Direct",
-                "storageIn": 123,
-                "storageOut": 23424
-            }
-        ]
-    }
+  {
+    'date': 'Jul 1',
+    'components': [
+      {
+        'name': 'Google Analytics',
+        'containerOut': 2345,
+        'containerIn': 234324,
+        'storageIn': 43453,
+        'storageOut': 0
+      },
+      {
+        'name': 'GoodData Writer',
+        'containerOut': 2345,
+        'containerIn': 234324,
+        'storageIn': 0,
+        'storageOut': 23424
+      },
+      {
+        'name': 'Transformation',
+        'containerOut': 0,
+        'containerIn': 0,
+        'storageIn': 0,
+        'storageOut': 23424
+      },
+      {
+        'name': 'Geneea',
+        'containerOut': 12334,
+        'containerIn': 12313,
+        'storageIn': 234324,
+        'storageOut': 23424,
+        'appUsage': 234
+      },
+      {
+        'name': 'Storage Direct',
+        'storageIn': 123,
+        'storageOut': 23424
+      }
+    ]
+  },
+  {
+    'date': 'Jul 2',
+    'components': [
+      {
+        'name': 'Google Analytics',
+        'containerOut': 2345,
+        'containerIn': 234324,
+        'storageIn': 43453,
+        'storageOut': 0
+      },
+      {
+        'name': 'GoodData Writer',
+        'containerOut': 2345,
+        'containerIn': 234324,
+        'storageIn': 0,
+        'storageOut': 23424
+      },
+      {
+        'name': 'Transformation',
+        'containerOut': 0,
+        'containerIn': 0,
+        'storageIn': 0,
+        'storageOut': 23424
+      },
+      {
+        'name': 'Storage Direct',
+        'storageIn': 123,
+        'storageOut': 23424
+      }
+    ]
+  }
 ]);
 
 export default React.createClass({
 
-    render() {
-        return (
-            <div className="container-fluid kbc-main-content">
-                <ul className="nav nav-tabs">
-                    <li role="presentation">
-                        <a href={this.projectPageUrl('settings-users')}>Users</a>
-                    </li>
-                    <li role="presentation">
-                        <a href={this.projectPageUrl('settings')}>Settings</a>
-                    </li>
-                    <li role="presentation">
-                        <a href={this.projectPageUrl('settings-limits')}>Limits</a>
-                    </li>
-                    <li role="presentation" className="active">
-                        <a href={this.projectPageUrl('settings-billing')}>Billing</a>
-                    </li>
-                </ul>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Data In</th>
-                            <th>Data Out</th>
-                            <th>Storage In</th>
-                            <th>Storage Out</th>
-                            <th>Application Credits</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map(this.dayRow)}
-                    </tbody>
-                </table>
-            </div>
-        );
-    },
-
-    dayRow(day) {
-        const header = (
+  render() {
+    return (
+      <div className="container-fluid kbc-main-content">
+        <ul className="nav nav-tabs">
+          <li role="presentation">
+            <a href={this.projectPageUrl('settings-users')}>Users</a>
+          </li>
+          <li role="presentation">
+            <a href={this.projectPageUrl('settings')}>Settings</a>
+          </li>
+          <li role="presentation">
+            <a href={this.projectPageUrl('settings-limits')}>Limits</a>
+          </li>
+          <li role="presentation" className="active">
+            <a href={this.projectPageUrl('settings-billing')}>Billing</a>
+          </li>
+        </ul>
+        <table className="table">
+          <thead>
           <tr>
-              <td><strong>{day.get('date')}</strong></td>
-              <td>
-                  <strong>
-                      <FileSize size={daySum(day.get('components'), 'containerIn')} />
-                  </strong>
-              </td>
-              <td>
-                  <strong>
-                      <FileSize size={daySum(day.get('components'), 'containerOut')} />
-                  </strong>
-              </td>
-              <td>
-                  <strong>
-                      <FileSize size={daySum(day.get('components'), 'storageIn')} />
-                  </strong>
-              </td>
-              <td>
-                  <strong>
-                      <FileSize size={daySum(day.get('components'), 'storageOut')} />
-                  </strong>
-              </td>
-              <td>
-                  <strong>
-                      {daySum(day.get('components'), 'appUsage')}
-                  </strong>
-              </td>
+            <th>Date</th>
+            <th>Data In</th>
+            <th>Data Out</th>
+            <th>Storage In</th>
+            <th>Storage Out</th>
+            <th>Application Credits</th>
           </tr>
-        );
-        const components = day.get('components').map(this.componentRow);
+          </thead>
+          <tbody>
+          {data.map(this.dayRow)}
+          </tbody>
+        </table>
+      </div>
+    );
+  },
 
-        return List([header, components]);
-    },
+  dayRow(day) {
+    const header = (
+      <tr>
+        <td><strong>{day.get('date')}</strong></td>
+        <td>
+          <strong>
+            <FileSize size={daySum(day.get('components'), 'containerIn')}/>
+          </strong>
+        </td>
+        <td>
+          <strong>
+            <FileSize size={daySum(day.get('components'), 'containerOut')}/>
+          </strong>
+        </td>
+        <td>
+          <strong>
+            <FileSize size={daySum(day.get('components'), 'storageIn')}/>
+          </strong>
+        </td>
+        <td>
+          <strong>
+            <FileSize size={daySum(day.get('components'), 'storageOut')}/>
+          </strong>
+        </td>
+        <td>
+          <strong>
+            {daySum(day.get('components'), 'appUsage')}
+          </strong>
+        </td>
+      </tr>
+    );
+    const components = day.get('components').map(this.componentRow);
 
-    componentRow(component) {
-        return (
-          <tr>
-              <td><span style={{paddingLeft: '10px'}}>{component.get('name')}</span></td>
-              <td>
-                  <FileSize size={component.get('containerIn')}/>
-              </td>
-              <td>
-                  <FileSize size={component.get('containerOut')}/>
-              </td>
-              <td>
-                  <FileSize size={component.get('storageIn')}/>
-              </td>
-              <td>
-                  <FileSize size={component.get('storageOut')}/>
-              </td>
-              <td>
-                  {component.get('appUsage')}
-              </td>
-          </tr>
-        );
-    },
+    return List([header, components]);
+  },
 
-
-    projectPageUrl(path) {
-        return ApplicationStore.getProjectPageUrl(path);
-    }
+  componentRow(component) {
+    return (
+      <tr>
+        <td><span style={{paddingLeft: '10px'}}>{component.get('name')}</span></td>
+        <td>
+          <FileSize size={component.get('containerIn')}/>
+        </td>
+        <td>
+          <FileSize size={component.get('containerOut')}/>
+        </td>
+        <td>
+          <FileSize size={component.get('storageIn')}/>
+        </td>
+        <td>
+          <FileSize size={component.get('storageOut')}/>
+        </td>
+        <td>
+          {component.get('appUsage')}
+        </td>
+      </tr>
+    );
+  },
+  
+  projectPageUrl(path) {
+    return ApplicationStore.getProjectPageUrl(path);
+  }
 
 });
