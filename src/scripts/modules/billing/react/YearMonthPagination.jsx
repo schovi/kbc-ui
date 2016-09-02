f7341e1eb24b4d3ddfa881b30cd9777cefad9d1d
Vladimír Kriška <vlado@keboola.com>
2016-09-02 10:31:50 +0200

    (settings-billing) Add helper class and custom pagination

---
+++ src/scripts/modules/billing/react/YearMonthPagination.jsx
@@ -0,0 +1,53 @@
+import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
+import {getPreviousYearMonth, getNextYearMonth} from '../helpers';
+
+export default React.createClass({
+
+  propTypes: {
+    min: PropTypes.string.isRequired,
+    max: PropTypes.string.isRequired,
+    current: PropTypes.string.isRequired
+  },
+
+  getPreviousLink() {
+    if (this.props.min !== this.props.current) {
+      return (
+        <Link to="settings-billing-month" params={{month: getPreviousYearMonth(this.props.current)}} className="btn btn-primary">
+          &larr; Previous month
+        </Link>
+      );
+    } else {
+      return (
+        <button className="btn btn-primary btn-disabled" disabled="disabled">
+          &larr; Previous month
+        </button>
+      );
+    }
+  },
+
+  getNextLink() {
+    if (this.props.max !== this.props.current) {
+      return (
+        <Link to="settings-billing-month" params={{month: getNextYearMonth(this.props.current)}} className="btn btn-primary">
+          Next month &rarr;
+        </Link>
+      );
+    } else {
+      return (
+        <button className="btn btn-primary btn-disabled" disabled="disabled">
+          Next month &rarr;
+        </button>
+      );
+    }
+  },
+
+  render() {
+    return (
+      <div>
+        {this.getPreviousLink()} {this.getNextLink()}
+      </div>
+    );
+  }
+
+});
