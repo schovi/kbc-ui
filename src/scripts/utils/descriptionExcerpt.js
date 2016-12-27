2558bf0dc84cc0815bd2641f2f801c1acc98d54e
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2016-12-27 19:18:38 +0100

    fix(descriptionExcerpt): handle invalid input values

--- src/scripts/utils/descriptionExcerpt.js
+++ src/scripts/utils/descriptionExcerpt.js
@@ -2,6 +2,9 @@ import removeMarkdown from 'remove-markdown';
 
 // remove markdown markup and trim to 100 characters
 export default function(description) {
+  if (description === null || description === undefined) {
+    return '';
+  }
   var plainText = removeMarkdown(description);
   if (plainText.length > 100) {
     plainText = plainText.substring(0, 100) + '...';
