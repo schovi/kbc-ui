a3330b9ca3a72f4e6e56a0119b696afe35216223
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-01-30 11:35:51 +0100

    fix(parseBuckets): check for null value
    FIXES #785

--- src/scripts/modules/transformations/utils/parseBuckets.js
+++ src/scripts/modules/transformations/utils/parseBuckets.js
@@ -2,8 +2,10 @@ import parseBucket from './parseBucket';
 
 export default function(buckets) {
   var response = [];
-  for (var i = 0; i < buckets.length; i++) {
-    response[i] = parseBucket(buckets[i]);
+  if (buckets) {
+    for (var i = 0; i < buckets.length; i++) {
+      response[i] = parseBucket(buckets[i]);
+    }
   }
   return response;
 }
