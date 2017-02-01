7b81097ae6a2e0a2fbc8a52a3ecb2f57d0173c19
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-02-01 16:01:45 +0100

    fix(keboola.ex-s3): icon in header

--- src/scripts/modules/ex-s3/routes.js
+++ src/scripts/modules/ex-s3/routes.js
@@ -1,6 +1,5 @@
 import Index from './react/pages/Index/Index';
 import storageActions from '../components/StorageActionCreators';
-import InstalledComponentsStore from '../components/stores/InstalledComponentsStore';
 import installedComponentsActions from '../components/InstalledComponentsActionCreators';
 import versionsActions from '../components/VersionsActionCreators';
 import jobsActions from '../jobs/ActionCreators';
@@ -8,14 +7,10 @@ import jobsActions from '../jobs/ActionCreators';
 const COMPONENT_ID = 'keboola.ex-s3';
 
 export default {
-  name: COMPONENT_ID + '-config',
+  name: COMPONENT_ID,
   path: ':config',
   isComponent: true,
   defaultRouteHandler: Index,
-  title: (routerState) => {
-    const configId = routerState.getIn(['params', 'config']);
-    return InstalledComponentsStore.getConfig(COMPONENT_ID, configId).get('name');
-  },
   poll: {
     interval: 10,
     action: (params) => jobsActions.loadComponentConfigurationLatestJobs(COMPONENT_ID, params.config)
