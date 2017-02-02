36bbf5bf1dd1a654800d968dbf0aa08e1ba6e8ae
Tomas Kacur <tomas.kacur@keboola.com>
2017-02-02 11:15:14 +0100

    fix(ex-fb): placeholder templates

---
+++ src/scripts/modules/ex-facebook/templates/placeholderTemplates.js
@@ -0,0 +1,12 @@
+export default {
+  'keboola.ex-facebook': {
+    'name': 'feed',
+    'path': 'feed',
+    'fields': 'caption,message,created_time,type,description'
+  },
+  'keboola.ex-facebook-ads': {
+    'name': 'ads',
+    'path': 'ads',
+    'fields': 'id,name,adset_id'
+  }
+};
